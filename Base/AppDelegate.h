26024cc79d05f9b4c0e1ce334f23e822691c41ed
lawrence <law@xqopen.com>
2016-10-27 14:51:08 +0800

    Initial Commit

---
+++ Base/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Base
+//
+//  Created by lawrence on 16/10/27.
+//  Copyright © 2016年 李辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Base/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Base
+//
+//  Created by lawrence on 16/10/27.
+//  Copyright © 2016年 李辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
